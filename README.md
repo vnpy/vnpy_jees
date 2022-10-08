c5261451fc2581842eb1d6f9f0d1e4b0ada3d93f
vn.py <xiaoyou.chen@foxmail.com>
2022-10-08 08:16:21 +0800

    [Mod] 初始化项目结构

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# vnpy_jees
-VeighNa框架的杰宜斯资管接口
+# VeighNa框架的CTP底层接口
+
+<p align="center">
+  <img src ="https://vnpy.oss-cn-shanghai.aliyuncs.com/vnpy-logo.png"/>
+</p>
+
+<p align="center">
+    <img src ="https://img.shields.io/badge/version-6.6.7.0-blueviolet.svg"/>
+    <img src ="https://img.shields.io/badge/platform-windows|linux-yellow.svg"/>
+    <img src ="https://img.shields.io/badge/python-3.7|3.8|3.9|3.10-blue.svg" />
+    <img src ="https://img.shields.io/github/license/vnpy/vnpy.svg?color=orange"/>
+</p>
+
+## 说明
+
+基于CTP期货版的6.6.7接口封装开发，接口中自带的是【穿透式实盘环境】的dll文件。
+
+## 安装
+
+安装环境推荐基于3.3.0版本以上的【[**VeighNa Studio**](https://www.vnpy.com)】。
+
+直接使用pip命令：
+
+```
+pip install vnpy_jees
+```
+
+
+或者下载源代码后，解压后在cmd中运行：
+
+```
+pip install .
+```
+
+使用源代码安装时需要进行C++编译，因此在执行上述命令之前请确保已经安装了【Visual Studio（Windows）】或者【GCC（Linux）】编译器。
+
+## 使用
+
+以脚本方式启动（script/run.py）：
+
+```
+from vnpy.event import EventEngine
+from vnpy.trader.engine import MainEngine
+from vnpy.trader.ui import MainWindow, create_qapp
+
+from vnpy_jees import CtpGateway
+
+
+def main():
+    """主入口函数"""
+    qapp = create_qapp()
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+    main_engine.add_gateway(CtpGateway)
+    
+    main_window = MainWindow(main_engine, event_engine)
+    main_window.showMaximized()
+
+    qapp.exec()
+
+
+if __name__ == "__main__":
+    main()
+```
---
+++ setup.py
@@ -0,0 +1,57 @@
+import platform
+
+from setuptools import Extension, setup
+
+
+def get_ext_modules() -> list:
+    """
+    获取三方模块
+    Linux和Windows需要编译封装接口
+    Mac由于缺乏二进制库支持无法使用
+    """
+    if platform.system() == "Linux":
+        extra_compile_flags = [
+            "-std=c++17",
+            "-O3",
+            "-Wno-delete-incomplete",
+            "-Wno-sign-compare",
+        ]
+        extra_link_args = ["-lstdc++"]
+        runtime_library_dirs = ["$ORIGIN"]
+
+    elif platform.system() == "Windows":
+        extra_compile_flags = ["-O2", "-MT"]
+        extra_link_args = []
+        runtime_library_dirs = []
+
+    else:
+        return []
+
+    vnctpmd = Extension(
+        name="vnpy_jees.api.vnctpmd",
+        sources=["vnpy_jees/api/vnctp/vnctpmd/vnctpmd.cpp"],
+        include_dirs=["vnpy_jees/api/include", "vnpy_jees/api/vnctp"],
+        library_dirs=["vnpy_jees/api/libs", "vnpy_jees/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=extra_compile_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        language="cpp",
+    )
+
+    vnctptd = Extension(
+        name="vnpy_jees.api.vnctptd",
+        sources=["vnpy_jees/api/vnctp/vnctptd/vnctptd.cpp"],
+        include_dirs=["vnpy_jees/api/include", "vnpy_jees/api/vnctp"],
+        library_dirs=["vnpy_jees/api/libs", "vnpy_jees/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=extra_compile_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        language="cpp",
+    )
+
+    return [vnctptd, vnctpmd]
+
+    
+setup(ext_modules=get_ext_modules())
